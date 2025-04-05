2f8f62132eb6bc64de9a856ccf1af1e43148a253
achyuta22 <achyuta22@DESKTOP-C4UL3E7>
2025-04-05 18:47:13 +0530

    image

--- ts/cluster1/nft_image.ts
+++ ts/cluster1/nft_image.ts
@@ -1,16 +1,19 @@
-import wallet from "../wba-wallet.json"
+import wallet from "./wallet/turbin3-wallet.json";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
+import { createGenericFile, createGenericFileFromBrowserFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
 import { readFile } from "fs/promises"
+import { File } from "buffer";
 
 // Create a devnet connection
 const umi = createUmi('https://api.devnet.solana.com');
+//  const umi=createUmi("https://turbine-solanad-4cde.devnet.rpcpool.com/168dd64f-ce5e-4e19-a836-f6482ad6b396");
 
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 
-umi.use(irysUploader());
+//  umi.use(irysUploader());
+ umi.use(irysUploader());
 umi.use(signerIdentity(signer));
 
 (async () => {
@@ -19,10 +22,16 @@ umi.use(signerIdentity(signer));
         //2. Convert image to generic file.
         //3. Upload image
 
-        // const image = ???
-
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
+         const imageBuffer =await readFile("./jefff.png")
+        //  const genericFile = createGenericFile(image, "nft.png", {
+        //     contentType: "image/png", // Change to image/jpeg if it's a JPEG
+        //   });
+        const image=new File([imageBuffer],"image.png",{
+            type:"image/png"
+        });
+        const file=await createGenericFileFromBrowserFile(image)
+          const [uri] = await umi.uploader.upload([file]); 
+         console.log("Your image URI: ", uri);
     }
     catch(error) {
         console.log("Oops.. Something went wrong", error);
--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -1,6 +1,6 @@
-import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../wba-wallet.json"
-import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
+import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, sendAndConfirmTransaction, Transaction } from "@solana/web3.js"
+import wallet from "./wallet/turbin3-wallet.json";
+import { createTransferInstruction, getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
 
 // We're going to import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -10,18 +10,42 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint =  new PublicKey("9jqRxQYVku8NSUhSorQ3gN27S5qdBBKDbLvY6YQ8ZHuC");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("57gWoxs5JqHwzDa2YTDiuVrA19ZkCMRhEQ6MHUbAoJLj");
 
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
 
         // Get the token account of the toWallet address, and if it does not exist, create it
+        const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
+            connection,
+            keypair,       // payer
+            mint,             // token mint
+            keypair.publicKey // owner of the token
+          );
+          
+          const toTokenAccount = await getOrCreateAssociatedTokenAccount(
+            connection,
+            keypair,        // payer
+            mint,              // token mint
+            to  // receiver
+          );
 
         // Transfer the new token to the "toTokenAccount" we just created
+        const transaction = new Transaction().add(
+            createTransferInstruction(
+              fromTokenAccount.address,
+              toTokenAccount.address,
+              keypair.publicKey,
+              1000  // = 10 tokens with 100 decimals
+            ),
+          );
+         const result= await sendAndConfirmTransaction(connection, transaction, [keypair]);
+         console.log(result)
+
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
